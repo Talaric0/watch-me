37f936ad8504b37fbf716c563f550ee9cc7d5448
bruno talarico <bruno.talarico@gmail.com>
2021-03-16 09:52:37 +0000

    done
    Signed-off-by: bruno talarico <bruno.talarico@gmail.com>

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+export function SideBar({ setSelectedGenreId, selectedGenreId, genres }) {
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre: GenreResponseProps) => (
+          <Button
+            key={genre.id}
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
